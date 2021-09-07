e9f7a089560649e34ee34ed7b8aabea1dd14bfde
bakolaszlo <bakolaszlo999@gmail.com>
2021-09-07 11:59:38 +0300

    Added testing unit.

---
+++ AspNetSandbox.Tests/BooksServiceTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace AspNetSandbox.Tests
+{
+    public class BooksServiceTests
+    {
+        [Fact]
+        public void BookIdTests()
+        {
+        // Assume
+
+        var bookService = new BooksService();
+
+            // Act
+
+            bookService.Post(new Book
+            {
+                Title = "Az isteni formula3",
+                Language = "Hungarian",
+                Author = "José Rodrigues dos Santos"
+            });
+
+            bookService.Delete(2);
+
+            bookService.Post(new Book
+            {
+                Title = "Az isteni formula4",
+                Language = "Hungarian",
+                Author = "José Rodrigues dos Santos"
+            });
+
+            // Assert
+            Assert.Equal("Az isteni formula3",bookService.Get(3).Title);
+
+        }
+    }
+}
