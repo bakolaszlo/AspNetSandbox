25e7df2d2564e0d24717e6ee7e0d00996e0bdacb
bakolaszlo <bakolaszlo999@gmail.com>
2021-09-08 20:52:58 +0300

    Made BooksService.cs acceptable for StyleCop.

--- AspNetSandbox/BooksService.cs
+++ AspNetSandbox/BooksService.cs
@@ -1,15 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿// <copyright file="BooksService.cs" company="P33">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
 namespace AspNetSandbox
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    /// <summary>Service for BooksController.</summary>
     public class BooksService : IBooksService
     {
         private List<Book> books;
         private int bookIdCounter = 1;
 
+        /// <summary>Initializes a new instance of the <see cref="BooksService" /> class.</summary>
         public BooksService()
         {
             books = new List<Book>();
@@ -19,7 +25,7 @@ namespace AspNetSandbox
                 Id = bookIdCounter++,
                 Title = "Az isteni formula",
                 Language = "Hungarian",
-                Author = "José Rodrigues dos Santos"
+                Author = "José Rodrigues dos Santos",
             });
 
             books.Add(new Book
@@ -27,15 +33,20 @@ namespace AspNetSandbox
                 Id = bookIdCounter++,
                 Title = "Az isteni formula2",
                 Language = "Hungarian",
-                Author = "José Rodrigues dos Santos"
+                Author = "José Rodrigues dos Santos",
             });
         }
 
+        /// <summary>Gets this instance.</summary>
+        /// <returns>IEnumerable of type Book.</returns>
         public IEnumerable<Book> Get()
         {
             return books;
         }
 
+        /// <summary>Gets the specified Book by identifier.</summary>
+        /// <param name="id">The identifier.</param>
+        /// <returns>Book object.</returns>
         public Book Get(int id)
         {
             try
@@ -48,12 +59,17 @@ namespace AspNetSandbox
             }
         }
 
+        /// <summary>Adds the book.</summary>
+        /// <param name="newBook">The new book.</param>
         public void AddBook(Book newBook)
         {
             newBook.Id = bookIdCounter++;
             books.Add(newBook);
         }
 
+        /// <summary>Updates the book by identifier.</summary>
+        /// <param name="id">The identifier.</param>
+        /// <param name="newBook">The new book.</param>
         public void UpdateBookById(int id, Book newBook)
         {
             newBook.Id = id;
@@ -67,6 +83,8 @@ namespace AspNetSandbox
             }
         }
 
+        /// <summary>Removes the book by identifier.</summary>
+        /// <param name="id">The identifier.</param>
         public void RemoveBookById(int id)
         {
             books.Remove(Get(id));
