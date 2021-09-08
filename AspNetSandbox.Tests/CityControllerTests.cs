535ea99845912d42af5047207bd2ea7d9ec6e311
bakolaszlo <bakolaszlo999@gmail.com>
2021-09-08 21:14:59 +0300

    Made CityControllerTests.cs acceptable for StyleCop.

--- AspNetSandbox.Tests/CityControllerTests.cs
+++ AspNetSandbox.Tests/CityControllerTests.cs
@@ -1,64 +1,65 @@
-﻿using AspNetSandbox.Controllers;
-using System;
-using System.IO;
-using Xunit;
+﻿// <copyright file="CityControllerTests.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
 namespace AspNetSandbox.Tests
 {
+    using System;
+    using System.IO;
+    using AspNetSandbox.Controllers;
+    using Xunit;
+
+    /// <summary>Test units for CityController.</summary>
     public class CityControllerTests
     {
-
+        /// <summary>CityController Test for London as city.</summary>
         [Fact]
         public void CityControllerTestLondon()
         {
-            //Assume
+            // Assume
             string content = LoadJsonFromResource("LondonApiResponse");
             var controller = new CityCoordController();
 
-            //Act
+            // Act
             var output = controller.ConvertCityNameResponseToCityObject(content);
 
-            //Assert
+            // Assert
             Assert.Equal("London", output.cityName);
-            Assert.Equal(-0.1257f,output.longtitude); // Postman: "lon": -0.1257, "lat": 51.5085
+            Assert.Equal(-0.1257f, output.longtitude);
             Assert.Equal(51.5085f, output.latitude);
         }
 
+        /// <summary>CityController Test for Brasov as city.</summary>
         [Fact]
         public void CityControllerTestBrasov()
         {
             // Assume
-
             string content = LoadJsonFromResource("BrasovApiResponse");
             var controller = new CityCoordController();
 
             // Act
             var output = controller.ConvertCityNameResponseToCityObject(content);
 
-
-
             // Assert
-            Assert.Equal("Brasov", output.cityName); //{"coord":{"lon":25.3333,"lat":45.75}
+            Assert.Equal("Brasov", output.cityName);
             Assert.Equal(25.3333f, output.longtitude);
             Assert.Equal(45.75f, output.latitude);
-
         }
 
-
+        /// <summary>CityController Test for Berlin as city.</summary>
         [Fact]
         public void CityControllerTestBerlin()
         {
-            //Assume
+            // Assume
             string content = LoadJsonFromResource("BerlinApiResponse");
             var controller = new CityCoordController();
 
-            //Act
+            // Act
             var output = controller.ConvertCityNameResponseToCityObject(content);
 
-            //Assert
-
+            // Assert
             Assert.Equal("Berlin", output.cityName);
-            Assert.Equal(13.4105f,output.longtitude); // Postman "lon":13.4105,"lat":52.5244
+            Assert.Equal(13.4105f, output.longtitude);
             Assert.Equal(52.5244f, output.latitude);
         }
 
@@ -73,6 +74,5 @@ namespace AspNetSandbox.Tests
                 return tr.ReadToEnd();
             }
         }
-
     }
 }
