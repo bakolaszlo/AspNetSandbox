4d7e7abc2dcec9b1992492deae4323a9e33e36a0
bakolaszlo <bakolaszlo999@gmail.com>
2021-09-16 22:13:16 +0300

    Made error.cshtml.cs acceptable by StyleCop.

--- AspNetSandbox/Pages/Error.cshtml.cs
+++ AspNetSandbox/Pages/Error.cshtml.cs
@@ -13,21 +13,30 @@ using Microsoft.Extensions.Logging;
 
 namespace AspNetSandbox.Pages
 {
+    /// <summary>Error model class for razor pages.</summary>
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     [IgnoreAntiforgeryToken]
     public class ErrorModel : PageModel
     {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-
         private readonly ILogger<ErrorModel> logger;
 
+        /// <summary>Initializes a new instance of the <see cref="ErrorModel" /> class.</summary>
+        /// <param name="logger">The logger.</param>
         public ErrorModel(ILogger<ErrorModel> logger)
         {
             this.logger = logger;
         }
 
+        /// <summary>Gets or sets the request identifier.</summary>
+        /// <value>The request identifier.</value>
+        public string RequestId { get; set; }
+
+        /// <summary>Gets a value indicating whether [show request identifier].</summary>
+        /// <value>
+        ///   <c>true</c> if [show request identifier]; otherwise, <c>false</c>.</value>
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+        /// <summary>Called when [get].</summary>
         public void OnGet()
         {
             RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
