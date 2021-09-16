3f057da87746c459e519628e67160df727d3c720
bakolaszlo <bakolaszlo999@gmail.com>
2021-09-16 22:24:12 +0300

    Made BookProfile.cs acceptable by StyleCop.

--- AspNetSandbox/Profiles/BookProfile.cs
+++ AspNetSandbox/Profiles/BookProfile.cs
@@ -2,23 +2,27 @@
 // Copyright (c) P33. All rights reserved.
 // </copyright>
 
-using AspNetSandbox.Dtos;
-using AspNetSandbox.Models;
-using AutoMapper;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using AspNetSandbox.Dtos;
+using AspNetSandbox.Models;
+using AutoMapper;
 
 namespace AspNetSandbox.Profiles
 {
+    /// <summary>
+    ///   <para>Profiles for automapper.</para>
+    /// </summary>
     public class BookProfile : Profile
     {
+        /// <summary>Initializes a new instance of the <see cref="BookProfile" /> class.</summary>
         public BookProfile()
         {
             CreateMap<CreateBookDto, Book>();
             CreateMap<Book, CreateBookDto>();
-            CreateMap<Book, ReadBookDto>();            
+            CreateMap<Book, ReadBookDto>();
         }
     }
 }
