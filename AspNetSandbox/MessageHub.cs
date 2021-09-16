ee80e20d30ff40822e1513ab39a66f5c29c00f7e
bakolaszlo <bakolaszlo999@gmail.com>
2021-09-16 22:03:59 +0300

    Made MessageHub acceptable by StyleCop.

--- AspNetSandbox/MessageHub.cs
+++ AspNetSandbox/MessageHub.cs
@@ -7,8 +7,13 @@ using Microsoft.AspNetCore.SignalR;
 
 namespace AspNetSandbox
 {
+    /// <summary>Messaging class.</summary>
     public class MessageHub : Hub
     {
+        /// <summary>Sends the message.</summary>
+        /// <param name="user">The user.</param>
+        /// <param name="message">The message.</param>
+        /// <returns>A <see cref="Task"/> representing the asynchronous operation. </returns>
         public async Task SendMessage(string user, string message)
         {
             await Clients.All.SendAsync("ReceiveMessage", user, message);
