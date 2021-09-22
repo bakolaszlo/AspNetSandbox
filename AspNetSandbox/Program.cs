64f8bd14592996b662161a1ab85c42b5237b6fc9
bakolaszlo <bakolaszlo999@gmail.com>
2021-09-22 18:49:27 +0300

    Added command line in properties args.

--- AspNetSandbox/Program.cs
+++ AspNetSandbox/Program.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using CommandLine;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -18,11 +19,28 @@ namespace AspNetSandbox
     /// </summary>
     public class Program
     {
+
         /// <summary>Defines the entry point of the application.</summary>
         /// <param name="args">The arguments.</param>
-        public static void Main(string[] args)
+        /// <returns>The program exit code.</returns>
+        public static int Main(string[] args)
         {
+            Parser.Default.ParseArguments<Options>(args)
+                   .WithParsed<Options>(o =>
+                   {
+                       if (o.Verbose)
+                       {
+                           Console.WriteLine($"Verbose output enabled. Current Arguments: -v {o.Verbose}");
+                           Console.WriteLine("Quick Start Example! App is in Verbose mode!");
+                       }
+                       else
+                       {
+                           Console.WriteLine($"Current Arguments: -v {o.Verbose}");
+                           Console.WriteLine("Quick Start Example!");
+                       }
+                   });
             CreateHostBuilder(args).Build().Run();
+            return 0;
         }
 
         /// <summary>Creates the host builder.</summary>
@@ -36,5 +54,21 @@ namespace AspNetSandbox
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+        /// <summary>
+        ///   <para>Class from CommandLineParser</para>
+        ///   <para>
+        ///     <font color="#333333">Makes the cmd verbose.</font>
+        ///   </para>
+        /// </summary>
+
+        public class Options
+        {
+            /// <summary>Gets or sets a value indicating whether this <see cref="Options" /> is verbose.</summary>
+            /// <value>
+            ///   <c>true</c> if verbose; otherwise, <c>false</c>.</value>
+            [Option('v', "verbose", Required = false, HelpText = "Set output to verbose messages.")]
+            public bool Verbose { get; set; }
+        }
     }
 }
